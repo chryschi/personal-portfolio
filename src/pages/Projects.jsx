412bbd222ffc67a5a35f6a6168671e8ad7da7b29
chryschi <121822197+chryschi@users.noreply.github.com>
2024-01-02 19:23:21 +0100

    Add prototype layout for project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,20 @@
+import "../App.css";
+
 const Projects = () => {
-  return <>Explore my projects!</>;
+  return (
+    <>
+      <div className="project-triangle-container">
+        <div className="triangle position-one small-up"></div>
+        <div className="triangle position-two medium-down"></div>
+        <div className="triangle position-three large-up"></div>
+        <div className="triangle position-four medium-down"></div>
+        <div className="triangle position-five large-down"></div>
+        <div className="triangle position-six medium-up"></div>
+        <div className="triangle position-seven small-down"></div>
+        <div className="triangle position-eight medium-up"></div>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
