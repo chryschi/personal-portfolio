631b89ae60aa63d833996847c0c6f28cd0518250
chryschi <121822197+chryschi@users.noreply.github.com>
2024-01-03 16:12:02 +0100

    Add slide animation to contact button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,19 @@
 const Contact = () => {
-  return <> Looking forward to your message!</>;
+  return (
+    <>
+      <div className="contact-container">
+        <div className="triangle medium-down contact"></div>
+        <div className="contact-triangle">
+          <p>
+            Looking forward to <br /> your message!
+          </p>
+          <div className="button-container">
+            <button>Contact Me</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
