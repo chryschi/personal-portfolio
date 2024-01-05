1664c46207a2bc99a685c057788ce9ffd81656b4
chryschi <121822197+chryschi@users.noreply.github.com>
2024-01-05 15:25:48 +0100

    Arrange triangles on projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,16 +4,18 @@ const Projects = () => {
   return (
     <>
       <div className="project-triangle-container">
-        <div className="triangle position-one small-up"></div>
-        <div className="triangle position-two medium-down"></div>
-        <div className="triangle position-three large-up"></div>
-        <div className="triangle position-four medium-down"></div>
-        <div className="triangle position-five large-down"></div>
-        <div className="triangle position-six medium-up"></div>
-        <div className="triangle position-seven small-down"></div>
-        <div className="triangle position-eight medium-up"></div>
-        <div className="triangle position-seven small-down"></div>
-        <div className="triangle position-eight medium-up"></div>
+        <div className="project-row row-one">
+          <div className="triangle position-one xs-up"></div>
+          <div className="triangle position-two small-down"></div>
+          <div className="triangle position-three medium-up"></div>
+          <div className="triangle position-four small-down"></div>
+        </div>
+        <div className="project-row row-two">
+          <div className="triangle position-five medium-down"></div>
+          <div className="triangle position-six small-up"></div>
+          <div className="triangle position-seven xs-down"></div>
+          <div className="triangle position-eight small-up"></div>
+        </div>
       </div>
     </>
   );
