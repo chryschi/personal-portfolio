3ebfe76c5a115aaf86e4f4766ed6d731d0e34681
chryschi <121822197+chryschi@users.noreply.github.com>
2024-01-06 00:56:47 +0100

    Use new implementation on Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <>
       <div className="contact-container">
-        <div className="triangle medium-down contact"></div>
+        <div className="triangle-medium triangle-down contact"></div>
         <div className="contact-triangle">
           <p>
             I&apos;m looking forward to <br /> your message!
