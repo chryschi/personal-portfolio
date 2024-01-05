a5ac8fbe6537f0294e061c9eb0dea6afd6ef4056
chryschi <121822197+chryschi@users.noreply.github.com>
2024-01-05 22:59:03 +0100

    Change CSS implementation of the triangles

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,16 +5,16 @@ const Projects = () => {
     <>
       <div className="project-triangle-container">
         <div className="project-row row-one">
-          <div className="triangle position-one xs-up"></div>
-          <div className="triangle position-two small-down"></div>
-          <div className="triangle position-three medium-up"></div>
-          <div className="triangle position-four small-down"></div>
+          <div className="triangle-xs position-one triangle-up"></div>
+          <div className="triangle-small position-two triangle-down"></div>
+          <div className="triangle-medium position-three triangle-up"></div>
+          <div className="triangle-small position-four triangle-down"></div>
         </div>
         <div className="project-row row-two">
-          <div className="triangle position-five medium-down"></div>
-          <div className="triangle position-six small-up"></div>
-          <div className="triangle position-seven xs-down"></div>
-          <div className="triangle position-eight small-up"></div>
+          <div className="triangle-medium position-five triangle-down"></div>
+          <div className="triangle-small position-six triangle-up"></div>
+          <div className="triangle-xs position-seven triangle-down"></div>
+          <div className="triangle-small position-eight triangle-up"></div>
         </div>
       </div>
     </>
