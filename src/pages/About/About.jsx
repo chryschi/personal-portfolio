a5c08a7793c6f162e7767e3c2d950dc26d3bdbb4
chryschi <121822197+chryschi@users.noreply.github.com>
2024-07-30 13:36:22 +0200

    Scroll to next about paragraph on click

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,64 @@
 import "./About.css";
 import portrait from "../../assets/projects_screenshots/portrait.jpg";
+import { useRef, useEffect, useState } from "react";
+
+const FadeInSection = ({ children, refFetcher }) => {
+  const [isVisible, setVisible] = useState(false);
+
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+    if (isVisible) {
+      refFetcher(domRef);
+    }
+    return () => observer.disconnect();
+  }, [refFetcher, isVisible]);
+
+  return (
+    <div
+      className={`fade-in-section carousel-item ${
+        isVisible ? "is-visible" : ""
+      }`}
+      ref={domRef}
+    >
+      {children}
+    </div>
+  );
+};
 
 const About = () => {
-  // const aboutMeInfo = [
+  const [focusedRef, setFocusedRef] = useState(null);
 
-  // ]
+  const handleScroll = () => {
+    focusedRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const getRef = (ref) => {
+    setFocusedRef(ref);
+  };
 
   return (
     <>
-      <div className="about-container">
+      <div onClick={handleScroll} className="about-container">
+        <div className="about-carousel-container">
+          <FadeInSection refFetcher={getRef}>
+            <p>Hi!</p>
+            <p>
+              {"I'm Abigail,"} <br />a web developer <br />
+              in the making!
+            </p>
+          </FadeInSection>
+          <FadeInSection refFetcher={getRef}>
+            I enjoy solving problems, <br />
+            producing music and being <br />
+            creative in general.
+          </FadeInSection>
+        </div>
+
         <img
           src={portrait}
           className="profile-photo triangle-medium triangle-down"
