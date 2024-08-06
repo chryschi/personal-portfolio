b26ffde65ef11d5219eef235e1c880ae1810bb14
chryschi <121822197+chryschi@users.noreply.github.com>
2024-08-06 22:43:22 +0200

    Change project order

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -2,100 +2,12 @@ import "./Projects.css";
 import { useContext, useEffect } from "react";
 import { AppContext } from "../../App";
 import ProjectCard from "../../components/ProjectCard";
-import imgCVBuilder from "../../assets/projects_screenshots/cv-builder-screenshot.png";
-import imgEasyCalc from "../../assets/projects_screenshots/easy-calculator.png";
-import imgAdminDashboard from "../../assets/projects_screenshots/admin-dashboard.png";
+
 import useViewport from "../../components/useViewport";
+import { firstRow, secondRow } from "./projectsInfo.jsx";
 
 const Projects = () => {
   const { grayscale } = useContext(AppContext);
-
-  const firstRow = [
-    {
-      classes:
-        "triangle-small position-one triangle-up project-triangle small-offset-up",
-      squareClasses: "project-square",
-      githubUrl: "",
-      projectUrl: "",
-      projectTitle: "",
-      imageUrl: "",
-      overlayColor: null,
-    },
-    {
-      classes:
-        "triangle-small position-two triangle-down project-triangle small-offset-down",
-      squareClasses: "project-square",
-      githubUrl: "",
-      projectUrl: "",
-      projectTitle: "",
-      imageUrl: "",
-      overlayColor: null,
-    },
-    {
-      classes:
-        "triangle-medium position-three triangle-up project-triangle medium-offset-up",
-      squareClasses: "project-square",
-      githubUrl: "https://github.com/chryschi/cv-builder ",
-      projectUrl: "https://cv-builder-chryschi.netlify.app",
-      projectTitle: "CV-Builder",
-      imageUrl: imgCVBuilder,
-      overlayColor: "rgb(255, 0, 0)",
-    },
-    {
-      classes:
-        "triangle-small position-four triangle-down project-triangle small-offset-down",
-      squareClasses: "project-square",
-      githubUrl: "",
-      projectUrl: "",
-      projectTitle: "",
-      imageUrl: "",
-      overlayColor: null,
-    },
-  ];
-
-  const secondRow = [
-    {
-      classes:
-        "triangle-medium position-five triangle-down project-triangle medium-offset-down",
-      squareClasses: "project-square",
-
-      githubUrl: "https://github.com/chryschi/calculator",
-      projectUrl: "https://chryschi.github.io/calculator/",
-      projectTitle: "",
-      imageUrl: imgEasyCalc,
-      overlayColor: "rgb(0, 0, 255)",
-    },
-    {
-      classes:
-        "triangle-small position-six triangle-up project-triangle small-offset-up",
-      squareClasses: "project-square",
-      githubUrl: "https://github.com/chryschi/admin-dashboard/",
-      projectUrl: "https://chryschi.github.io/admin-dashboard/",
-      projectTitle: "",
-      imageUrl: imgAdminDashboard,
-      overlayColor: null,
-    },
-    {
-      classes:
-        "triangle-small position-seven triangle-down project-triangle small-offset-down",
-      squareClasses: "project-square",
-      githubUrl: "",
-      projectUrl: "",
-      projectTitle: "",
-      imageUrl: "",
-      overlayColor: null,
-    },
-    {
-      classes:
-        "triangle-small position-eight triangle-up project-triangle small-offset-up",
-      squareClasses: "project-square",
-      githubUrl: "",
-      projectUrl: "",
-      projectTitle: "",
-      imageUrl: "",
-      overlayColor: null,
-    },
-  ];
 
   const { width } = useViewport();
   const breakpoint = 1016;
@@ -108,9 +20,6 @@ const Projects = () => {
 
   return (
     <>
-      {/* <div
-        className={"projects-background" + (grayscale ? " grayscale" : "")}
-      ></div> */}
       <div
         className={
           width < breakpoint
---
+++ src/pages/Projects/projectsInfo.jsx
@@ -0,0 +1,89 @@
+import imgCVBuilder from "../../assets/projects_screenshots/cv-builder-screenshot.png";
+import imgEasyCalc from "../../assets/projects_screenshots/easy-calculator.png";
+import imgAdminDashboard from "../../assets/projects_screenshots/admin-dashboard.png";
+
+export const firstRow = [
+  {
+    classes:
+      "triangle-small position-one triangle-up project-triangle small-offset-up",
+    squareClasses: "project-square",
+    githubUrl: "https://github.com/chryschi/cv-builder ",
+    projectUrl: "https://cv-builder-chryschi.netlify.app",
+    projectTitle: "CV-Builder",
+    imageUrl: imgCVBuilder,
+    overlayColor: "darkmagenta",
+  },
+  {
+    classes:
+      "triangle-small position-two triangle-down project-triangle small-offset-down",
+    squareClasses: "project-square",
+    githubUrl: "",
+    projectUrl: "",
+    projectTitle: "",
+    imageUrl: "",
+    overlayColor: "orange",
+  },
+  {
+    classes:
+      "triangle-medium position-three triangle-up project-triangle medium-offset-up",
+    squareClasses: "project-square",
+    githubUrl: "https://github.com/chryschi/calculator",
+    projectUrl: "https://chryschi.github.io/calculator/",
+    projectTitle: "Easy Calculator",
+    imageUrl: imgEasyCalc,
+    overlayColor: "rgb(255, 0, 0)",
+  },
+  {
+    classes:
+      "triangle-small position-four triangle-down project-triangle small-offset-down",
+    squareClasses: "project-square",
+    githubUrl: "https://github.com/chryschi/admin-dashboard/",
+    projectUrl: "https://chryschi.github.io/admin-dashboard/",
+    projectTitle: "Admin Dashboard",
+    imageUrl: imgAdminDashboard,
+    overlayColor: "pink",
+  },
+];
+
+export const secondRow = [
+  {
+    classes:
+      "triangle-medium position-five triangle-down project-triangle medium-offset-down",
+    squareClasses: "project-square",
+    githubUrl: "",
+    projectUrl: "",
+    projectTitle: "",
+    imageUrl: "",
+    overlayColor: "rgb(0, 0, 255)",
+  },
+  {
+    classes:
+      "triangle-small position-six triangle-up project-triangle small-offset-up",
+    squareClasses: "project-square",
+    githubUrl: "",
+    projectUrl: "",
+    projectTitle: "",
+    imageUrl: "",
+    overlayColor: "teal",
+  },
+  {
+    classes:
+      "triangle-small position-seven triangle-down project-triangle small-offset-down",
+    squareClasses: "project-square",
+    githubUrl: "",
+    projectUrl: "",
+    projectTitle: "",
+    imageUrl: "",
+    overlayColor: "brown",
+  },
+  {
+    classes:
+      "triangle-small position-eight triangle-up project-triangle small-offset-up",
+    squareClasses: "project-square",
+    githubUrl: "",
+    projectUrl: "",
+    projectTitle: "",
+    imageUrl: "",
+    overlayColor: "turquoise",
+  },
+];
