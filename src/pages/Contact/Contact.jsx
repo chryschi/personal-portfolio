3b6505b2e9fb996247df62f07c683a45c5dd8843
chryschi <121822197+chryschi@users.noreply.github.com>
2024-08-06 15:49:18 +0200

    Adjust contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,7 +2,6 @@ const Contact = () => {
   return (
     <>
       <div className="contact-container">
-        <div className="triangle-medium triangle-down contact"></div>
         <div className="contact-triangle">
           <p>
             I&apos;m looking forward to <br /> your message!
