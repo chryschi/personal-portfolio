2df40b3902ec5374be76edb7a74b310469de0436
chryschi <121822197+chryschi@users.noreply.github.com>
2024-08-18 21:12:14 +0200

    Disable scrollbar on reaching bottom of about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
   const [tooltipTranslate, setTooltipTranslate] = useState(0);
   const [isTransitioning, setIsTransitioning] = useState(false);
   const [activeElementIdx, setActiveElementIdx] = useState(0);
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(10);
   const textRef = useRef(null);
 
   const ELEMENT_HEIGHT = 320;
@@ -34,30 +34,42 @@ const About = () => {
       const position = textRef.current.getBoundingClientRect().bottom - height;
       setScrollPosition(position);
     };
+    if (scrollPosition === 0) {
+      document.body.style.overflowY = "hidden";
 
-    window.addEventListener("scroll", getScrollPosition, { passive: true });
-    console.log(textRef.current.getBoundingClientRect().bottom - height);
-    setTranslateY(activeElementIdx * -ELEMENT_HEIGHT);
-
-    return window.removeEventListener("scroll", getScrollPosition);
-  }, [activeElementIdx, height]);
-
-  const handleScroll = throttle((e) => {
-    // logic for scrolling down
-    if (activeElementIdx < 3) {
-      if (e.deltaY > 0) {
-        setIsTransitioning(true);
-        setActiveElementIdx((prev) => prev + 1);
-        setTooltipTranslate((prev) => prev + 100);
+      if (activeElementIdx === 0) {
+        document.body.style.overflowY = "scroll";
       }
     }
 
-    //logic for scrolling up
-    if (activeElementIdx > 0) {
-      if (e.deltaY < 0) {
-        setIsTransitioning(true);
-        setActiveElementIdx((prev) => prev - 1);
-        setTooltipTranslate((prev) => prev - 100);
+    document.body.addEventListener("scroll", getScrollPosition);
+    console.log(textRef.current.getBoundingClientRect().bottom - height);
+    setTranslateY(activeElementIdx * -ELEMENT_HEIGHT);
+
+    return () => {
+      document.body.removeEventListener("scroll", getScrollPosition);
+      document.body.style.overflowY = "scroll";
+    };
+  }, [activeElementIdx, height, scrollPosition]);
+
+  const handleScroll = throttle((e) => {
+    if (scrollPosition <= 1) {
+      // logic for scrolling down
+      if (activeElementIdx < 3) {
+        if (e.deltaY > 0) {
+          setIsTransitioning(true);
+          setActiveElementIdx((prev) => prev + 1);
+          setTooltipTranslate((prev) => prev + 100);
+        }
+      }
+
+      //logic for scrolling up
+      if (activeElementIdx > 0) {
+        if (e.deltaY < 0) {
+          setIsTransitioning(true);
+          setActiveElementIdx((prev) => prev - 1);
+          setTooltipTranslate((prev) => prev - 100);
+        }
       }
     }
   }, 100);
