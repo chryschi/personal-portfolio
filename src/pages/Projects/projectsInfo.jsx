8b1d1b69d8fb1e0a0462f23269c3cece731468a4
chryschi <121822197+chryschi@users.noreply.github.com>
2024-10-14 15:21:58 +0200

    Add project "DJ Library" to portfolio

--- src/pages/Projects/projectsInfo.jsx
+++ src/pages/Projects/projectsInfo.jsx
@@ -2,46 +2,47 @@ import imgCVBuilder from "../../assets/projects_screenshots/cv-builder-screensho
 import imgEasyCalc from "../../assets/projects_screenshots/easy-calculator.png";
 import imgAdminDashboard from "../../assets/projects_screenshots/admin-dashboard.png";
 import imgMpPortfolio from "../../assets/projects_screenshots/mp-portfolio.png";
+import imgDJLibrary from "../../assets/projects_screenshots/dj-library.png";
 
 export const firstRow = [
   {
     classes:
       "triangle-small position-one triangle-up project-triangle small-offset-up",
     squareClasses: "project-square",
-    githubUrl: "https://github.com/chryschi/cv-builder ",
-    projectUrl: "https://cv-builder.abigailmajor.com",
-    projectTitle: "CV-Builder",
-    imageUrl: imgCVBuilder,
+    githubUrl: "https://github.com/chryschi/dj-library-management",
+    projectUrl: "https://neighbouring-kyla-chryschi-ead69390.koyeb.app/",
+    projectTitle: "DJ Library",
+    imageUrl: imgDJLibrary,
     overlayColor: "darkmagenta",
   },
   {
     classes:
       "triangle-small position-two triangle-down project-triangle small-offset-down",
     squareClasses: "project-square",
-    githubUrl: "https://github.com/chryschi/mp-portfolio-new",
-    projectUrl: "https://mp-portfolio.abigailmajor.com",
-    projectTitle: "MP Portfolio Website",
-    imageUrl: imgMpPortfolio,
+    githubUrl: "https://github.com/chryschi/cv-builder ",
+    projectUrl: "https://cv-builder.abigailmajor.com",
+    projectTitle: "CV-Builder",
+    imageUrl: imgCVBuilder,
     overlayColor: "orange",
   },
   {
     classes:
       "triangle-medium position-three triangle-up project-triangle medium-offset-up",
     squareClasses: "project-square",
-    githubUrl: "https://github.com/chryschi/calculator",
-    projectUrl: "https://chryschi.github.io/calculator/",
-    projectTitle: "Easy Calculator",
-    imageUrl: imgEasyCalc,
+    githubUrl: "https://github.com/chryschi/mp-portfolio-new",
+    projectUrl: "https://mp-portfolio.abigailmajor.com",
+    projectTitle: "MP Portfolio Website",
+    imageUrl: imgMpPortfolio,
     overlayColor: "rgb(255, 0, 0)",
   },
   {
     classes:
       "triangle-small position-four triangle-down project-triangle small-offset-down",
     squareClasses: "project-square",
-    githubUrl: "https://github.com/chryschi/admin-dashboard/",
-    projectUrl: "https://chryschi.github.io/admin-dashboard/",
-    projectTitle: "Admin Dashboard",
-    imageUrl: imgAdminDashboard,
+    githubUrl: "https://github.com/chryschi/calculator",
+    projectUrl: "https://chryschi.github.io/calculator/",
+    projectTitle: "Easy Calculator",
+    imageUrl: imgEasyCalc,
     overlayColor: "pink",
   },
 ];
@@ -51,10 +52,10 @@ export const secondRow = [
     classes:
       "triangle-medium position-five triangle-down project-triangle medium-offset-down",
     squareClasses: "project-square",
-    githubUrl: "",
-    projectUrl: "",
-    projectTitle: "",
-    imageUrl: "",
+    githubUrl: "https://github.com/chryschi/admin-dashboard/",
+    projectUrl: "https://chryschi.github.io/admin-dashboard/",
+    projectTitle: "Admin Dashboard",
+    imageUrl: imgAdminDashboard,
     overlayColor: "rgb(0, 0, 255)",
   },
   {
